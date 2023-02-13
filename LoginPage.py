3df3ae137a992c3467b5dbeca01ec9d78c623319
UyaoJan <79205692+UyaoJan@users.noreply.github.com>
2023-02-14 01:00:45 +0800

    this is life

--- LoginPage.py
+++ LoginPage.py
@@ -63,8 +63,6 @@ class Loginpage():
         self.Password=StringVar()
         self.Password_Entry=Entry(self.LoginFrame2,width=40,textvariable=password,show="*",borderwidth=3,).place(x=50,y=200)
 
-        self.Forget=Label(self.LoginFrame2,text="Forget Password:",font='Roboto 8').place(x=60,y=253)
-        self.Chick_Here=Label(self.LoginFrame2,text="Check Here",fg="blue",font='Roboto 8').place(x=150,y=253)
         self.Login_Enter=Button(self.LoginFrame2,text="ENTER",width=10,bg="green", command=self.loginLogic)
         self.Login_Enter.place(x=220,y=250)
         self.LoginGUI.mainloop()
